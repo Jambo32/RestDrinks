87d54ae285eaa55470f64533ea37dc95b75840b1
Jamie Sandison <jamie.sandison@yahoo.co.uk>
2022-04-24 18:22:16 +0100

    updated with an arraylist and added some tests

--- src/main/java/com/DrinksRest/RestDrinks/DrinksController.java
+++ src/main/java/com/DrinksRest/RestDrinks/DrinksController.java
@@ -1,6 +1,8 @@
 package com.DrinksRest.RestDrinks;
-
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
 
 @RestController
 public class DrinksController {
@@ -62,23 +64,71 @@ public class DrinksController {
                 " " + measurement;
     }
 
+    /**
+     * url?myX=5&myY=8 returns 13
+     * investigate curl for calling webservices
+     * investigate json for sending back data (done!)
+     * learn how to use post unit test
+     * how to send response code e.g 200 for success
+     * create gradle and maven file
+     * look up web forms (done!)
+     * @param myX
+     * @param myY
+     * @return
+     */
+
+    @GetMapping("/Drinks/value")
+    @ResponseBody
+    public int getValue(@RequestParam String myX, @RequestParam String myY) {
+
+        int x = Integer.getInteger(myX, 10);
+        int y = Integer.getInteger(myY, 22);
+        return y + x;
+    }
+
+    /**
+     * need to junit test the method below
+     * it works but still need to run tests!
+     * @return
+     */
+
+    @GetMapping("/Drinks/arrayDrinks")
+    @ResponseBody
+    public ArrayList getDrinkNames () {
+
+        ArrayList<String> drinkNames = new ArrayList<>();
+        drinkNames.add("mai tai");
+        drinkNames.add("japanese slipper");
+        drinkNames.add("zombie");
+        drinkNames.add("b52");
+        drinkNames.add("miami vice");
+
+        return drinkNames;
+    }
+
+    @GetMapping("/Drinks/drinkChoice")
+    @ResponseBody
+    public String getDrink(@RequestParam String cocktail) {
+        return "Drink: " + cocktail;
+    }
+
     @GetMapping("/Drinks/Get")
-    public String getDrink() {
+    public String getMethodDrink() {
         return "HTTP GET drink request was sent";
     }
 
     @PutMapping("/Drinks/Put")
-    public String updateDrink() {
+    public String updateMethodDrink() {
         return "HTTP PUT drink request has been sent";
     }
 
     @PostMapping("/Drinks/Post")
-    public String createADrink() {
+    public String createMethodDrink() {
         return "HTTP POST drink request has been sent";
     }
 
     @DeleteMapping("/Drinks/Delete")
-    public String deleteDrink() {
+    public String deleteMethodDrink() {
         return "HTTP DELETE drink request has been sent";
     }
 
--- src/test/java/com/DrinksRest/RestDrinks/RestDrinksApplicationTests.java
+++ src/test/java/com/DrinksRest/RestDrinks/RestDrinksApplicationTests.java
@@ -1,14 +1,40 @@
 package com.DrinksRest.RestDrinks;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
-class RestDrinksApplicationTests {
+class restDrinksApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	/**
+	 * getValueTest should receive a value of 32
+	 */
+
+	@Test
+	void getValueTest() {
+
+		DrinksController drinksController = new DrinksController();
+		Assertions.assertEquals(32, drinksController.getValue("10", "22"));
+	}
+
+	@Test
+	void getMappingResponseForGet(){
+
+		DrinksController drinksController = new DrinksController();
+		Assertions.assertSame("HTTP POST drink request has been sent", drinksController.createMethodDrink());
+	}
+
+	@Test
+	void drinksNamesArrayList(){
+
+		DrinksController drinks = new DrinksController();
+		Assertions.assertEquals(5, drinks.getDrinkNames().size());
+		Assertions.assertTrue("mai tai" == drinks.getDrinkNames().get(0));
+		Assertions.assertTrue("miami vice" == drinks.getDrinkNames().get(4));
+		Assertions.assertFalse(drinks.getDrinkNames().contains("pina colada"));
+	}
 }
